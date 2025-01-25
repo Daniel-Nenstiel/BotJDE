876309b632e974fdac8df8a711e7a8cb4bd8dece
Daniel-Nenstiel <daniel.nenstiel@vertexinc.com>
2025-01-25 12:28:19 -0500

    Fix for MyBatis mapper

--- src/main/java/run/scatter/botjde/entity/User.java
+++ src/main/java/run/scatter/botjde/entity/User.java
@@ -13,6 +13,14 @@ public class User {
   private Snowflake userId;
   private Snowflake serverId;
 
+  // MyBatis Constructor
+  public User(String name, String username, Long userId, Long serverId) {
+    this.name = name;
+    this.username = username;
+    this.userId = Snowflake.of(userId);
+    this.serverId = Snowflake.of(serverId);
+  }
+
   public void setDiscordId(Long discordId) {
     this.userId = Snowflake.of(discordId);
   }
