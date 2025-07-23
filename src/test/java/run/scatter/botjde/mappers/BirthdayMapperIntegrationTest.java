25cdfc717a2b652f4ca1eaadaf8a10312e9441f6
DJ Nenstiel <Daniel.Nenstiel@gmail.com>
2025-07-23 12:02:58 -0400

    Fix scheduled messages (#3)
    * Refactor User class and update BirthdayMapperIntegrationTest with correct SQL statements

--- src/test/java/run/scatter/botjde/mappers/BirthdayMapperIntegrationTest.java
+++ src/test/java/run/scatter/botjde/mappers/BirthdayMapperIntegrationTest.java
@@ -5,10 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Tag("integration")
+@Sql(statements = {
+  "INSERT INTO people (serverId, userId, username, name) VALUES (123456789012345678, 234567890123456789, 'testuser', 'Test User');",
+  "INSERT INTO events (id, type, event_date) VALUES ('test-event-id', 'birthday', CURRENT_DATE);",
+  "INSERT INTO people_events (person_userId, person_serverId, event_id) VALUES (234567890123456789, 123456789012345678, 'test-event-id');"
+})
 class BirthdayMapperIntegrationTest {
   @Autowired
   private BirthdayMapper birthdayMapper;
@@ -16,6 +24,6 @@ class BirthdayMapperIntegrationTest {
   @Test
   void testGetTodaysBirthdaysForServerMapping() {
     // This will throw if the mapping is broken
-    birthdayMapper.getTodaysBirthdaysForServer("1");
+    birthdayMapper.getTodaysBirthdaysForServer("123456789012345678");
   }
 }
